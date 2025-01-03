67df8a27d5fec154923b740280fcbc328ae63222
Sofia Carlsson <sofia@celestus.se>
2025-01-03 14:22:47 +0100

    Add project files.

---
+++ Celestus.Storage.Cache.Test/TestThreadCache.cs
@@ -0,0 +1,233 @@
+﻿namespace Celestus.Storage.Cache.Test
+{
+    [TestClass]
+    [DoNotParallelize] // The tests are not threadsafe since they dispose of resource other tests use.
+    public sealed class TestThreadCache
+    {
+        const int DEFAULT_TIMEOUT = 1000;
+
+        private ThreadCache _cache = null!;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _cache = ThreadCache.CreateShared(nameof(TestThreadCache), DEFAULT_TIMEOUT);
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            if (!_cache.IsDisposed)
+            {
+                _cache.Dispose();
+            }
+        }
+
+        [TestMethod]
+        public void VerifyThatGetRetrievesSetValue()
+        {
+            //
+            // Arrange
+            //
+
+            //
+            // Act
+            //
+            const int VALUE = 55;
+            const string key = "key";
+
+            _ = _cache.TrySet(key, VALUE);
+
+            //
+            // Assert
+            //
+            Assert.IsTrue(_cache.TryGet<int>(key) is (true, VALUE));
+        }
+
+        [TestMethod]
+        public void VerifyThatSettingValueMultipleTimesWorks()
+        {
+            //
+            // Arrange
+            //
+
+            //
+            // Act
+            //
+            const int VALUE = 55;
+            const string KEY = "test";
+
+            _ = _cache.TrySet(KEY, VALUE);
+            _ = _cache.TrySet(KEY, VALUE + 1);
+            _ = _cache.TrySet(KEY, VALUE + 2);
+            _ = _cache.TrySet(KEY, VALUE + 3);
+            _ = _cache.TrySet(KEY, VALUE + 4);
+
+            //
+            // Assert
+            //
+            Assert.IsTrue(_cache.TryGet<int>(KEY) is (true, VALUE + 4));
+        }
+
+        [TestMethod]
+        public void VerifyThatCachesWithDifferentKeysAreUnique()
+        {
+            //
+            // Arrange
+            //
+            var otherCache = ThreadCache.CreateShared("other", DEFAULT_TIMEOUT);
+
+            const int VALUE = 55;
+            const string KEY = "test";
+            _ = _cache.TrySet(KEY, VALUE);
+
+            //
+            // Act
+            //
+            _ = otherCache.TrySet(KEY, VALUE + 1);
+            var otherResult = otherCache.TryGet<int>(KEY);
+            otherCache.Dispose();
+
+            //
+            // Assert
+            //
+            Assert.IsTrue(_cache.TryGet<int>(KEY) is (true, VALUE));
+            Assert.IsTrue(otherResult is (true, VALUE + 1));
+        }
+
+        [TestMethod]
+        public void VerifyThatNewCacheIsCreatedAfterFirstIsDisposed()
+        {
+            //
+            // Arrange
+            //
+
+            //
+            // Act
+            //
+            _cache.Dispose();
+            _cache = ThreadCache.CreateShared(nameof(TestThreadCache), DEFAULT_TIMEOUT);
+
+            //
+            // Assert
+            //
+            Assert.IsFalse(_cache.IsDisposed);
+        }
+
+        [TestMethod]
+        public void VerifyThatCacheCanBeDisposed()
+        {
+            //
+            // Arrange
+            //
+
+            //
+            // Act
+            //
+            _cache.Dispose();
+
+            //
+            // Assert
+            //
+            Assert.IsTrue(_cache.IsDisposed);
+        }
+
+        [TestMethod]
+        public void VerifyThatEightThreadsCanUseCacheInParallelWithDifferentKeys()
+        {
+            //
+            // Arrange
+            //
+            const int N_ITERATION = 1000;
+            const int N_THREADS = 80;
+
+            Action ThreadWorkerBuilder(int id)
+            {
+                return () =>
+                {
+                    var key = id.ToString();
+                    _ = _cache.TrySet(key, id);
+
+                    for (int i = 1; i <= N_ITERATION; i++)
+                    {
+                        var (result, value) = _cache.TryGet<int>(key);
+                        _ = _cache.TrySet(key, value + 1);
+                    }
+                };
+            }
+
+            var threads = Enumerable.Range(0, N_THREADS)
+                                    .Select(x => new Task(ThreadWorkerBuilder(x)))
+                                    .ToArray();
+
+            //
+            // Act
+            //
+            foreach (var thread in threads)
+            {
+                thread.Start();
+            }
+
+            const int THREAD_TEST_TIMEOUT = 10000;
+            _ = Task.WaitAll(threads, THREAD_TEST_TIMEOUT);
+
+            //
+            // Assert
+            //
+            for (int i = 0; i < N_THREADS; i++)
+            {
+                var key = i.ToString();
+                Assert.AreEqual(N_ITERATION + i, _cache.TryGet<int>(key).data);
+            }
+        }
+
+        [TestMethod]
+        public void VerifyThatMultipleThreadsCanInteractWithSameKeyWithoutCrashing()
+        {
+            //
+            // Arrange
+            //
+            const int N_ITERATION = 1000;
+            const int N_THREADS = 80;
+
+            var key = "hammer";
+
+            Action ThreadWorkerBuilder(int id)
+            {
+                return () =>
+                {
+                    _ = _cache.TrySet(key, id);
+
+                    for (int i = 1; i <= N_ITERATION; i++)
+                    {
+                        var (result, value) = _cache.TryGet<int>(key);
+                        _ = _cache.TrySet(key, value * (i + 1));
+                    }
+                };
+            }
+
+            var threads = Enumerable.Range(0, N_THREADS)
+                                    .Select(x => new Task(ThreadWorkerBuilder(x)))
+                                    .ToArray();
+
+            //
+            // Act
+            //
+            foreach (var thread in threads)
+            {
+                thread.Start();
+            }
+
+            //
+            // Assert
+            //
+
+            /* We do not care about anything but that nothing crashes.
+             * There is no way to assert on the final value due to race condtions
+             * between threads. */
+
+            const int THREAD_TEST_TIMEOUT = 10000;
+            _ = Task.WaitAll(threads, THREAD_TEST_TIMEOUT);
+        }
+    }
+}
---
+++ Celestus.Storage.Cache/ThreadCache.cs
@@ -0,0 +1,174 @@
+﻿using System.Runtime.Serialization;
+
+namespace Celestus.Storage.Cache
+{
+    public class ThreadCache : IDisposable, ISerializable
+    {
+        #region Factory Pattern
+        readonly static Dictionary<string, ThreadCache> _caches = [];
+
+        private static void RemoveDisposedCache(object? sender, string key)
+        {
+            lock (nameof(ThreadCache))
+            {
+                _ = _caches.Remove(key);
+            }
+        }
+
+        public static ThreadCache CreateShared(string key, int timeout)
+        {
+            lock (nameof(ThreadCache))
+            {
+                if (_caches.TryGetValue(key, out var cache) && !cache.IsDisposed)
+                {
+                    return cache;
+                }
+                else
+                {
+                    cache = new ThreadCache(key, timeout);
+                    cache.OnDisposed += RemoveDisposedCache;
+
+                    _caches[key] = cache;
+
+                    return cache;
+                }
+            }
+        }
+        #endregion
+        private record CacheEntry(DateTime Expiration, object? Data);
+
+        readonly ReaderWriterLockSlim _lock = new();
+        readonly Dictionary<string, CacheEntry> _storage = [];
+
+        public bool IsDisposed { get; set; } = false;
+        private event EventHandler<string>? OnDisposed;
+
+        readonly private string Key;
+        readonly private int Timeout;
+
+        private ThreadCache(string key, int timeout)
+        {
+            Key = key;
+            Timeout = timeout;
+
+            if (Timeout == 0)
+            {
+                throw new ArgumentException($"{nameof(timeout)} cannot be zero.");
+            }
+        }
+
+        public bool TrySet<DataType>(string key, DataType value, TimeSpan? duration = null)
+        {
+            if (_lock.TryEnterWriteLock(Timeout))
+            {
+                try
+                {
+                    DateTime expiratonDate = DateTime.MaxValue;
+
+                    if (duration != null && duration != TimeSpan.Zero)
+                    {
+                        _ = DateTime.Now.Add((TimeSpan)duration);
+                    }
+
+                    _storage[key] = new(expiratonDate, value);
+
+                    return true;
+                }
+                finally
+                {
+                    _lock.ExitWriteLock();
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public (bool result, DataType? data) TryGet<DataType>(string key)
+        {
+            if (!_lock.TryEnterReadLock(Timeout))
+            {
+                return (false, default);
+            }
+            else
+            {
+                try
+                {
+                    if (!_storage.TryGetValue(key, out var entry))
+                    {
+                        return (false, default);
+                    }
+                    else if (entry.Expiration < DateTime.Now)
+                    {
+                        return (false, default);
+                    }
+                    else if (entry.Data is not DataType data)
+                    {
+                        return (false, default);
+                    }
+                    else
+                    {
+                        return (true, data);
+                    }
+                }
+                finally
+                {
+                    _lock.ExitReadLock();
+                }
+            }
+        }
+
+        #region IDisposable
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!IsDisposed)
+            {
+                OnDisposed?.Invoke(this, Key);
+
+                if (disposing)
+                {
+                    _lock.Dispose();
+                }
+
+                IsDisposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        #endregion
+
+        #region ISerializable
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("Key", Key);
+            info.AddValue("Timeout", Timeout);
+            info.AddValue("Storage", _storage);
+        }
+
+        protected ThreadCache(SerializationInfo info, StreamingContext context)
+        {
+            Key = info.GetString("Key") ?? throw new SerializationException("Key cannot be null");
+
+            Timeout = info.GetInt32("Timeout");
+
+            if (Timeout == 0)
+            {
+                throw new SerializationException($"{nameof(Timeout)} cannot be 0.");
+            }
+
+            if (info.GetValue("Storage", typeof(Dictionary<string, CacheEntry>)) is Dictionary<string, CacheEntry> storage)
+            {
+                _storage = storage;
+            }
+            else
+            {
+                throw new SerializationException($"Storage is not a {nameof(Dictionary<string, CacheEntry>)}.");
+            }
+        }
+        #endregion
+    }
+}
