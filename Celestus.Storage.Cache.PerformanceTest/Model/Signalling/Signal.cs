5bdb831f72ec475f327be3b8b48ccd88a95a7bd0
Sofia Carlsson <sofia@celestus.se>
2025-01-27 13:51:31 +0100

    Add benchmark for BufferBlock.

---
+++ Celestus.Storage.Cache.PerformanceTest/Model/Signalling/Signal.cs
@@ -0,0 +1,18 @@
+﻿namespace Celestus.Storage.Cache.PerformanceTest.Model.Signalling
+{
+    enum SignalType
+    {
+        AdditionRequest,
+        AdditionResponse,
+        AdditionIndication,
+        Stop
+    }
+
+    record Signal(SignalType Type);
+
+    record SignalAdditionRequest(int ValueA, int ValueB) : Signal(SignalType.AdditionRequest);
+
+    record SignalAdditionIndication(int ValueA, int ValueB) : Signal(SignalType.AdditionIndication);
+
+    record SignalAdditionResponse(int Value) : Signal(SignalType.AdditionResponse);
+}
--- Celestus.Storage.Cache.PerformanceTest/Program.cs
+++ Celestus.Storage.Cache.PerformanceTest/Program.cs
@@ -1,4 +1,6 @@
 ﻿using BenchmarkDotNet.Running;
 using Celestus.Storage.Cache.PerformanceTest;
+using Celestus.Storage.Cache.PerformanceTest.Model.Signalling;
 
-var summary = BenchmarkRunner.Run<CacheBenchmark>();
+//var summary = BenchmarkRunner.Run<CacheBenchmark>();
+var summary = BenchmarkRunner.Run<BufferBlockBenchmark>();
